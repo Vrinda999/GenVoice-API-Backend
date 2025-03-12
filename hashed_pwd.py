3d49711dc2996402e725751370a1c397b404540a
Vrinda <abrolvrinda@gmail.com>
2025-03-12 11:00:50 +0530

    removed redundant function declaration and definitiion.

--- hashed_pwd.py
+++ hashed_pwd.py
@@ -6,17 +6,10 @@ I am Hashing the passwords and then storing them into the database, enabling sec
 Since I have Hashed the passwords directly before storing in the API code, I need to separately hash the dummy data passwords, and this is the Justification for the Same.
 '''
 
-import bcrypt
-
-def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-def check_password(hashed_password, plain_password):
-    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password)
+from utils.security import hash_password, check_password
 
 
-
-password = {"pwdjoel87": "", "pwshiminh": "", "pwrishiaw": ""}
+password = {"pwjoel87": "", "pwshiminh": "", "pwrishiaw": ""}
 
 for i in password:
     hashed_pwd = hash_password(i)
