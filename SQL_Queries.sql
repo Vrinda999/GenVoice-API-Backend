4f682e7afb4719b4858168e8095a29bb92431e0f
Vrinda <abrolvrinda@gmail.com>
2025-03-12 09:46:13 +0530

    feat: cases data.

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -1,5 +1,7 @@
 -- Create Database
-CREATE Database GenVoice -- Clinician
+CREATE Database GenVoice;
+--------------------------------------------------------------------------------------------------
+-- Clinician
 -- Table Creation
 CREATE TABLE clinicians (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,6 +10,7 @@ CREATE TABLE clinicians (
     Password VARCHAR(255) NOT NULL,
     Role ENUM('Senior', 'Junior') NOT NULL DEFAULT 'Junior'
 );
+-------------------------------------------------
 -- Storing Initial Data
 INSERT INTO clinicians
 VALUES(
@@ -31,3 +34,30 @@ VALUES(
         "b'$2b$12$ec4R9zw4fC7TpzUHX5XnDevZwhKT1kg.36.o53YZR3LSxDGXbQS0S'",
         "Junior"
     );
+--------------------------------------------------------------------------------------------------
+-- Cases
+-- Table Creation
+CREATE TABLE cases (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1000) NOT NULL
+);
+-------------------------------------------------
+-- Storing Initial Data
+INSERT INTO cases
+VALUES(
+        001,
+        "Jonathan Lim",
+        "A 28-year-old software engineer who is experiencing intense anxiety during team meetings and is struggling to speak up, fearing judgment from colleagues."
+    ),
+    (
+        002,
+        "Angela Paolo",
+        "A 42-year-old teacher who is coping with the recent loss of a parent and is finding it difficult to concentrate on work and daily responsibilities."
+    ),
+    (
+        003,
+        "Xu Yaoming",
+        "A 16-year-old high school student who is feeling overwhelmed by academic pressure and is struggling to balance schoolwork, extracurriculars, and personal time."
+    );
+--------------------------------------------------------------------------------------------------
